660208b2a4408ef360b17e5d6d200590c1e1cf31
Enow Mbi <benowmbi@yahoo.com>
2022-04-26 19:56:36 +0100

    Use stimulus to handle search with a half a second delay for productivity reasons

--- app/javascript/controllers/image_controller.js
+++ app/javascript/controllers/image_controller.js
@@ -2,7 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
     connect() {
-        console.log("Contacts controller connected !")
     }
 
     static targets = [ "output", "input" ]
