625752b58e55999976f9f9adc8be8fe30f0e863d
Enow Mbi <benowmbi@yahoo.com>
2022-04-26 18:10:17 +0100

    Add validation for presence of name, email and image types and size

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,24 @@
 class Contact < ApplicationRecord
   has_one_attached :image
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
-  # validates :name, presence: true, length: {within: (10..40)}, uniqueness: true
-  # validates :email, presence: true, 
+  validates :name, presence: true, length: { within: (10..40) }, uniqueness: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  validate :acceptable_image
+
+  private
+
+  def acceptable_image
+    return unless image.attached?
+
+    unless image.byte_size <= 1.megabyte
+      errors.add(:image, "is too big")
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(image.content_type)
+      errors.add(:image, "must be a JPEG or PNG")
+    end
+  end
 end
