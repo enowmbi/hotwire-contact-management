701a3579983bb5aff2ecfbf32032977e373882a3
Enow Mbi <benowmbi@yahoo.com>
2022-04-26 14:40:56 +0100

    Rename contacts.js controller to image.js

---
+++ app/javascript/controllers/image_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    connect() {
+        console.log("Contacts controller connected !")
+    }
+
+    static targets = [ "output", "input" ]
+
+    readURL() {
+        var input = this.inputTarget
+        var output = this.outputTarget
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function () {
+                output.src = reader.result
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+}
