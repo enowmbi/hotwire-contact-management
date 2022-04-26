4afd910583fbf5bf9e818e70b38a3b1cc5bb0321
Enow Mbi <benowmbi@yahoo.com>
2022-04-26 14:35:16 +0100

    Clean up styles and allow preview of images in form

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,5 @@
 module ContactsHelper
+  def preview_image(contact)
+    contact.image.present? ? contact.image : "preview.png"
+  end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ApplicationRecord
   has_one_attached :image
+
+  # validates :name, presence: true, length: {within: (10..40)}, uniqueness: true
+  # validates :email, presence: true, 
 end
